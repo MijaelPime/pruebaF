34337441006647033b95e62b95fe0863e8695d99
MijaelPime <39095192+MijaelPime@users.noreply.github.com>
2025-04-07 12:57:14 -0600

    Add files via upload

---
+++ Bienvenidos a la Banca en Línea _ BBVA México_files/w8tu8dsjr8eo9e5yd06gya.js
@@ -0,0 +1,11 @@
+;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+};p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
+n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","//w8tu8dsjr8eo9e5yd06gya.s.decidata.tv/v3.7.0.js","snowplow"));
+  
+snowplow("newTracker", "sp", "w8tu8dsjr8eo9e5yd06gya.t.decidata.tv", {
+  appId: "w8tu8dsjr8eo9e5yd06gya",
+  discoverRootDomain: !0,
+});
+
+snowplow("trackPageView");
